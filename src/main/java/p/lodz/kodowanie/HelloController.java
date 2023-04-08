0d525b78cbde050c8a8e56ef57c2a6cd74c130de
Jakub Januszewicz <94139663+boniffacy@users.noreply.github.com>
2023-04-08 17:24:44 +0200

    some gui progeress

--- src/main/java/p/lodz/kodowanie/HelloController.java
+++ src/main/java/p/lodz/kodowanie/HelloController.java
@@ -1,5 +1,6 @@
 package p.lodz.kodowanie;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.TextArea;
@@ -15,26 +16,45 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class HelloController {
+
+    Kodowanie coding = new Kodowanie();
     public TextArea encodedText;
     public TextArea decodedText;
     FileChooser fileChooser = new FileChooser();
 
     @FXML
-    protected void loadEncoded() throws IOException {
-        fileChooser.getExtensionFilters().addAll(
-                new FileChooser.ExtensionFilter("ALL FILES", "*.*"));
-        File file = fileChooser.showOpenDialog(null);
-        String content = Files.readString(Path.of(file.getAbsolutePath()), StandardCharsets.UTF_8);
-        encodedText.setText(content);
-    }
-
-    @FXML
-    protected void saveDecoded() throws IOException {
+    protected void saveEncoded() throws IOException {
         fileChooser.getExtensionFilters().addAll(
                 new FileChooser.ExtensionFilter("ALL FILES", "*.*"));
         File file = fileChooser.showOpenDialog(null);
         BufferedWriter writer = new BufferedWriter(new FileWriter(file.getAbsolutePath()));
-        writer.write(decodedText.getText());
+        writer.write(encodedText.getText());
         writer.close();
+    }
+
+    // znajdz metode zeby zapisywac do nowo tworzonego pliku(nie ze musi juz istniec)
+    @FXML
+    protected void loadDecoded() throws IOException {
+        fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("ALL FILES", "*.*"));
+        File file = fileChooser.showOpenDialog(null);
+        String content = Files.readString(Path.of(file.getAbsolutePath()), StandardCharsets.UTF_8);
+        decodedText.setText(content);
+    }
+
+    public void decode(ActionEvent actionEvent) throws IOException {
+        byte[] message = coding.readMessage("encoded.txt");
+        message = coding.decode(message);
+        coding.saveFileAsBytes(message, "decoded.txt");
+        String decodedMessage = Files.readString(Path.of("decoded.txt"));
+        decodedText.setText(decodedMessage);
+    }
+
+    public void encode(ActionEvent actionEvent) throws IOException {
+        byte[] message = decodedText.getText().getBytes();
+        message = coding.encode(message);
+        coding.saveFileAsBytes(message, "encoded.txt");
+        String encodedMessage = Files.readString(Path.of("encoded.txt"));
+        encodedText.setText(encodedMessage);
     }
 }
